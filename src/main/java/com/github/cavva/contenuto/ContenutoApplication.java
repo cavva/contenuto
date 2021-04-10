93a5da63708ebae79508ec2e4d686582b08ad726
cavva <carlo.cavallieri@gmail.com>
2021-04-10 10:13:07 +0000

    Init spring-boot

---
+++ src/main/java/com/github/cavva/contenuto/ContenutoApplication.java
@@ -0,0 +1,13 @@
+package com.github.cavva.contenuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContenutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContenutoApplication.class, args);
+	}
+
+}
